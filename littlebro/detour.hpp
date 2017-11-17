640776cb8d06bdf5fedf856a327208e73ffbbcae
Justas Masiulis <Justas.masiulis@yahoo.com>
2017-11-17 20:43:49 +0200

    replaced vector with std::array to avoid unnecessary allocations

--- littlebro/detour.hpp
+++ littlebro/detour.hpp
@@ -4,7 +4,7 @@
 class detour
 {
 public:
-	static std::vector<char> generate_shellcode(uintptr_t hook_pointer);
-	static void hook_function(uintptr_t function_address, uintptr_t hook_address);
-	static void remove_detour(uintptr_t function_address, char* original_bytes, size_t length);
+    static std::array<char, 0xF> generate_shellcode(uintptr_t hook_pointer);
+    static void hook_function(uintptr_t function_address, uintptr_t hook_address);
+    static void remove_detour(uintptr_t function_address, char* original_bytes, size_t length);
 };
