439b0375686f3d53e1f1b4e39d9aec8424c26f44
Justas Masiulis <Justas.masiulis@yahoo.com>
2017-11-18 12:33:13 +0200

    minor performance and code improvements to api_set

--- latebros/api_set.cpp
+++ latebros/api_set.cpp
@@ -3,24 +3,23 @@
 
 api_set::api_set()
 {
-	auto peb = reinterpret_cast<uintptr_t>(NtCurrentTeb()->ProcessEnvironmentBlock);
+	const auto peb = reinterpret_cast<uintptr_t>(NtCurrentTeb()->ProcessEnvironmentBlock);
 
 	auto api_set = *reinterpret_cast<API_SET_NAMESPACE_ARRAY**>(peb + 0x68);
 
-	for (size_t entry_index = 0; entry_index < api_set->count; ++entry_index)
+	for (ULONG entry_index = 0; entry_index < api_set->count; ++entry_index)
 	{
-		auto descriptor = api_set->entry(entry_index);
+		const auto descriptor = api_set->entry(entry_index);
 
-		wchar_t dll_name[MAX_PATH] = { 0 };
-		api_set->read_name(descriptor, dll_name);
-		std::transform(dll_name, dll_name + MAX_PATH, dll_name, ::tolower);
+		auto dll_name = api_set->get_name(descriptor);
+		std::for_each(dll_name.begin(), dll_name.end(), ::tolower);
 
-		auto host_data = api_set->get_host(descriptor);
+		const auto host_data = api_set->get_host(descriptor);
 
 		std::vector<std::wstring> hosts;
 		for (ULONG j = 0; j < host_data->count; j++)
 		{
-			auto host = host_data->entry(api_set, j);
+			const auto host = host_data->entry(api_set, j);
 
 			std::wstring host_name(reinterpret_cast<wchar_t*>(reinterpret_cast<uint8_t*>(api_set) + host->value_offset),
 				host->value_length / sizeof(wchar_t));
@@ -32,7 +31,7 @@ api_set::api_set()
 			}
 		}
 
-		this->schema.emplace(dll_name, hosts);
+		this->schema.emplace(std::move(dll_name), std::move(hosts));
 	}
 }
 
@@ -41,12 +40,12 @@ bool api_set::query(std::wstring& name)
 	// SEARCH FOR ANY ENTRIES OF OUR PROXY DLL
 	auto iter = std::find_if(this->schema.begin(), this->schema.end(), [name](const map_api_schema::value_type& val)
 	{
-		return name.find(val.first.c_str()) != name.npos;
+		return name.find(val.first) != name.npos;
 	});
 
 	if (iter != this->schema.end()) // FOUND
 	{
-		name = iter->second.front() != name ? iter->second.front() : iter->second.back();
+		name = (iter->second.front() != name ? iter->second.front() : iter->second.back());
 		return true;
 	}
 
